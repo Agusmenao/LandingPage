fd301c84c94c6164db81d5a5ffa5f5d0aab7b982
Agustín Menao <agusmenao@gmail.com>
2023-04-15 22:43:51 -0300

    acomodo del contenido de la pagina

--- landingpage.js
+++ landingpage.js
@@ -1,6 +1,27 @@
+
 function mostrar(){
     document.getElementById("OcultarForm").style.display = "block";
 }
 function ocultar(){
     document.getElementById("OcultarForm").style.display = "none";
+}
+
+const PrevBrn = document.querySelectorAll(".btn-atrás");
+const SigBtn = document.querySelectorAll(".btn-siguiente");
+const Progreso = document.getElementById("progreso");
+const PasosForm =document.querySelectorAll(".Formulariopaso1");
+
+let NumPasosDelForm = 0;
+
+SigBtn.forEach((btn) => {
+ btn.addEventListener("click", () => {
+    NumPasosDelForm++;
+    updatepasosForm();
+
+ });
+});
+
+function updatepasosFor() {
+    PasosForm[NumPasosDelForm].classList.add("formulario-pasos-activo");
+
 }
